94fe92d9445627162dea5adcb8335811aa65e066
lakshyaseth <seth.lakshya@gmail.com>
2016-11-04 20:35:37 +0800

    Update loadData.sh

--- loadData.sh
+++ loadData.sh
@@ -1,2 +1,5 @@
-cd /usr/local/bin/
-mongoimport --db d8 --collection order --type csv --headerline --file /Users/lakshyaseth/mongodb/d8/table1.csv
+cd /temp/mongodb/mongo/bin/                                                                                                                                                                             
+./mongoimport --db d8 --collection order --type csv --headerline --file /temp/mongodb/d8/table1.csv
+./mongoimport --db d8 --collection customer --type csv --headerline --file /temp/mongodb/d8/table2.csv
+./mongoimport --db d8 --collection wdpayment --type csv --headerline --file /temp/mongodb/d8/table3.csv
+./mongoimport --db d8 --collection stock --type csv --headerline --file /temp/mongodb/d8/table4.csv
